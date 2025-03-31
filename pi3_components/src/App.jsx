48c4db9ae2ca4400eb45dc0e24c7e7d82cc6cd4d
Renato Miguel Henriques Sobral <pv28267@alunos.estgv.ipv.pt>
2025-03-31 15:18:36 +0100

    Enhance App component structure and styling with new Navbar and Footer, and adjust footer padding

--- pi3_components/src/App.jsx
+++ pi3_components/src/App.jsx
@@ -1,5 +1,36 @@
-import React from "react";
-import Homepage from "./Homepage";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import logo from "./assets/logo.png";
+import Hero from "./components/Hero";
+import CardNews from "./components/CardNews";
+import HomeOportunidades from "./components/features/HomeOportunidades";
+import { Container } from "react-bootstrap"; // Import Container from react-bootstrap
+
+const heroContent =
+  "Bem-vindo à Plataforma de Emprego ESTGV, que conecta alunos e ex-alunos a oportunidades de trabalho/estágio. Cria o teu perfil, destaca as tuas competências e recebe recomendações de emprego adequadas às tuas metas profissionais. As empresas podem facilmente publicar ofertas, ajudando-te a encontrar a combinação perfeita para a tua carreira.";
+
+const info1content = "100+";
+const info1title = "Ofertas";
+
+const info2content = "50+";
+const info2title = "Empresas";
+
+const info3content = "2000+";
+const info3title = "Estudantes";
+
+const contactInfo = {
+  address: "Av. Cor. José Maria Vale de Andrade, Campus Politécnico, 3504-510 Viseu",
+  phone: "+351 232 480 700 (Chamadas para a rede fixa nacional)",
+  email: "ipv@sc.ipv.pt"
+};
+
+const socialLinks = {
+  instagram: "#",
+  facebook: "#",
+  youtube: "#"
+};
 
 function App() {
   return (
