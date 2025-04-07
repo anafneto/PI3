7e91f505f9ed3c3038e6468bf28cbfcc3a7a9e84
solana315 <ana.cdo315@gmail.com>
2025-04-07 12:41:17 +0100

    oportunidades

--- pi3_components/src/components/features/Oportunidades.jsx
+++ pi3_components/src/components/features/Oportunidades.jsx
@@ -5,7 +5,7 @@ const CardList = ({ cards }) => {
   return (
     <div className="cardop-container">
       {cards.map((card, index) => (
-        <div key={index} className={`cardop ${card.size === "small" ? "cardop-small" : ""}`}>
+        <div key={index} className={`cardop ${card.size === "small1" ? "cardop-small1" : ""} ${card.size === "big1" ? "cardop-big1" : ""} ${card.size === "big2" ? "cardop-big2" : ""} ${card.size === "big3" ? "cardop-big3" : ""} ${card.size === "small2" ? "cardop-small2" : ""}`}>
           <div className="cardop-icon">{card.icon}</div>
           <div>
             <h3 className="cardop-title">{card.title}</h3>
