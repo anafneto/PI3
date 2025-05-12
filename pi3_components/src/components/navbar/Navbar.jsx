df210ee9ab9763ab8cd2fbe65942f3c6632028f3
Renato Sobral <pv28267@alunos.estgv.ipv.pt>
2025-05-12 09:40:33 +0100

    Update dependencies and refactor Navbar component by integrating Button and Logo components directly

--- pi3_components/src/components/navbar/Navbar.jsx
+++ pi3_components/src/components/navbar/Navbar.jsx
@@ -1,7 +1,44 @@
-import Button from "./Button";
-import Logo from "./Logo";
 import { useNavigate } from "react-router-dom";
 
+// Button component
+const Button = ({ noColor, onClick, outlined, children }) => {
+  return (
+    <>
+      {noColor ? (
+        <button type="button" className="btn btn-sm" onClick={onClick}>
+          {children}
+        </button>
+      ) : outlined ? (
+        <button
+          type="button"
+          className="btn btn-outline-dark btn-sm"
+          onClick={onClick}
+        >
+          {children}
+        </button>
+      ) : (
+        <button type="button" className="btn btn-dark btn-sm" onClick={onClick}>
+          {children}
+        </button>
+      )}
+    </>
+  );
+};
+
+// Logo component
+const Logo = ({ logo, onClick }) => {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" onClick={onClick}>
+          <img src={logo} width="237px" height="32px" alt="" flex-shrink="0" />
+        </a>
+      </nav>
+    </>
+  );
+};
+
+// Navbar component
 const Navbar = ({ logo }) => {
   const navigate = useNavigate();
   const handleLogoClick = () => {
