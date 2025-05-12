28fa0f75c93567a8f5cda90747bd7e0aa6d3be70
solana315 <ana.cdo315@gmail.com>
2025-05-12 10:15:51 +0100

    footer melhorado

--- pi3_components/src/components/Footer.jsx
+++ pi3_components/src/components/Footer.jsx
@@ -8,37 +8,39 @@ import EmailIcon from "../assets/svg/email.svg";
 
 const Footer = ({ contactInfo, socialLinks }) => {
     return (
-        <footer className="footer-bgcolor text-light p-4">
-            <div className="container d-flex justify-content-left footer-gap">
-                <div>
-                    <h5>Contactos</h5>
-                    <p>
-                        <img src={LocationIcon} width="20" height="20" style={{ marginRight: '8px' }} /> 
-                        {contactInfo.address}
-                    </p>
-                    <p>
-                        <img src={PhoneIcon} width="20" height="20" style={{ marginRight: '8px' }} /> 
-                        {contactInfo.phone}
-                    </p>
-                    <p>
-                        <img src={EmailIcon} width="20" height="20" style={{ marginRight: '8px' }} /> 
-                        {contactInfo.email}
-                    </p>
-                </div>
-                <div>
-                    <h5>Redes Sociais</h5>
-                    <p>
-                        <img src={InstagramIcon} width="20" height="20" style={{ marginRight: '8px' }} />
-                        <a href={socialLinks.instagram} className="text-light">Instagram</a>
-                    </p>
-                    <p>
-                        <img src={FacebookIcon} width="20" height="20" style={{ marginRight: '8px' }} />
-                        <a href={socialLinks.facebook} className="text-light">Facebook</a>
-                    </p>
-                    <p>
-                        <img src={YoutubeIcon} width="20" height="20" style={{ marginRight: '8px' }} />
-                        <a href={socialLinks.youtube} className="text-light">Youtube</a>
-                    </p>
+        <footer className="footer-bgcolor text-light">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 mb-3">
+                        <h5>Contactos</h5>
+                        <p>
+                            <img src={LocationIcon} width="20" height="20" style={{ marginRight: '8px' }} alt="Localização" />
+                            {contactInfo.address}
+                        </p>
+                        <p>
+                            <img src={PhoneIcon} width="20" height="20" style={{ marginRight: '8px' }} alt="Telefone" />
+                            {contactInfo.phone}
+                        </p>
+                        <p>
+                            <img src={EmailIcon} width="20" height="20" style={{ marginRight: '8px' }} alt="Email" />
+                            {contactInfo.email}
+                        </p>
+                    </div>
+                    <div className="col-6 mb-3">
+                        <h5>Redes Sociais</h5>
+                        <p>
+                            <img src={InstagramIcon} width="20" height="20" style={{ marginRight: '8px' }} alt="Instagram" />
+                            <a href={socialLinks.instagram} className="text-light" target="_blank" rel="noopener noreferrer">Instagram</a>
+                        </p>
+                        <p>
+                            <img src={FacebookIcon} width="20" height="20" style={{ marginRight: '8px' }} alt="Facebook" />
+                            <a href={socialLinks.facebook} className="text-light" target="_blank" rel="noopener noreferrer">Facebook</a>
+                        </p>
+                        <p>
+                            <img src={YoutubeIcon} width="20" height="20" style={{ marginRight: '8px' }} alt="Youtube" />
+                            <a href={socialLinks.youtube} className="text-light" target="_blank" rel="noopener noreferrer">Youtube</a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </footer>
