51e9e625f88fe93e3e38d833939993db82bd6f9d
Renato Sobral <pv28267@alunos.estgv.ipv.pt>
2025-05-19 09:56:55 +0100

    Dashboard do admin mais organizada

---
+++ pi3_components/src/components/Smallbox.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const SmallBox = ({ color, value, label, iconPath, link }) => {
+  return (
+    <div className={`col-lg-3 col-6`}>
+      <div className={`small-box text-bg-${color}`}>
+        <div className="inner">
+          <h3>{value}</h3>
+          <p>{label}</p>
+        </div>
+        <svg
+          className="small-box-icon"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path d={iconPath}></path>
+        </svg>
+        <a
+          href={link}
+          className="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"
+        >
+          More info <i className="bi bi-link-45deg"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default SmallBox;
