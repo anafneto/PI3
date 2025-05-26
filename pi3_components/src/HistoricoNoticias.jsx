d37f9d6bf7ac45afd436676154dec1dabd7639b9
solana315 <ana.cdo315@gmail.com>
2025-05-26 11:35:14 +0100

    HISTORICONOTICIAS

--- pi3_components/src/HistoricoNoticias.jsx
+++ pi3_components/src/HistoricoNoticias.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./components/admin/Sidebar.jsx";
+import NewsTable from "./components/NoticiasHistorico/NoticiasTable.jsx";
 import Breadcrumb from "./components/Breadcrumb.jsx";
-import CardsNews from "./components/HistoricoNewsCards.jsx"; 
 
-export default function CreateManagerForm() {
+const HistoricoNoticias = () => {
+  const newsData = [
+    {
+      title: "Notícia 1",
+      date: "26/05/2025",
+      description: "Descrição da notícia 1.",
+    },
+    {
+      title: "Notícia 2",
+      date: "25/05/2025",
+      description: "Descrição da notícia 2.",
+    },
+  ];
+
   return (
     <div className="d-flex" style={{ minHeight: "100vh" }}>
       {/* Sidebar fixa */}
@@ -12,12 +24,17 @@ export default function CreateManagerForm() {
 
       {/* Conteúdo principal que ocupa o restante */}
       <div className="flex-grow-1 container py-5">
-        {/* Breadcrumb */}
-        <Breadcrumb />
 
-        {/* Cards de Propostas */}
-        <CardsNews />
+        {/* Breadcrumb */}
+       <Breadcrumb />
+        {/* Conteúdo principal */}
+        <div className="container mt-4 col-10 col-md-9 col-lg-10">
+          <NewsTable data={newsData} />
+        </div>
       </div>
-    </div>
+</div>
+
   );
-}
+};
+
+export default HistoricoNoticias;
